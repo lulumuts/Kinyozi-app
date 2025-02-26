7a71e66886aec46e30b1c74c29302d527cb1a5af
Lulu Mutuli <lulu.mutuli@regulaition.com>
2025-02-26 13:04:31 +0800

    vite build resolved

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { dirname, resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+build: {
+  outDir: 'dist'
+},
+server: {
+  port: 3000
+},
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
